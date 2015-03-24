e1c97910916d7acf43a1fdc9c8d5543a96f635b4
Elizabeth <ElizabethSanchez@crbc.net>
2015-03-24 11:13:26 -0400

    Initial Commit

---
+++ Variables/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Variables
+//
+//  Created by Liz Sanchez on 3/24/15.
+//  Copyright (c) 2015 Liz Sanchez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
